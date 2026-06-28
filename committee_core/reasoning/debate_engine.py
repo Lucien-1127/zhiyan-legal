6e9a2c5bf6d9a467d2ef46aa2642a5a9559760ea
Lucien <Lucien127@proton.me>
2026-06-28 18:57:22 +0000

    Refactor: Move committee logic to committee_core/ and establish governance replay schema

---
+++ committee_core/reasoning/debate_engine.py
@@ -0,0 +1,8 @@
+class DebateEngine:
+    """獨立的辯論引擎，不依賴任何領域知識"""
+    def __init__(self, models):
+        self.models = models
+
+    def orchestrate_debate(self, claim):
+        # 實作跨模型辯論邏輯
+        pass
